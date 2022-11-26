3751fef14b8e5d65192d648b002e0173b2fdafff
alex9 <66374743+piglex9@users.noreply.github.com>
2022-11-26 01:54:01 +0100

    Create hack.lua

---
+++ hack.lua
@@ -0,0 +1,25 @@
+--game link: https://www.roblox.com/games/4591043103/Marble-Rider-Star-Quest#!/about
+
+local workspace = game:GetService("Workspace")
+local players = game:GetService("Players")
+
+local player = players.LocalPlayer
+
+assert(firetouchinterest, "No firetouchinterest")
+assert(fireclickdetector, "No fireclickdetector")
+
+while true do
+    wait(1) -- make it wait longer if you have shit ping
+    if _G.stop then -- _G.stop = true to disable it
+        break
+    end
+    for i,v in next, workspace.Collect:GetDescendants() do
+        if v.Name == "Star" and v:FindFirstChild("TouchInterest") then
+            firetouchinterest(player.Character.HumanoidRootPart, v, 0)
+            firetouchinterest(player.Character.HumanoidRootPart, v, 1)
+        end
+        if v.Name == "Star" and v:FindFirstChild("ClickDetector") then
+            fireclickdetector(v.ClickDetector, math.huge)
+        end
+    end
+end
