87d05181f414d340c1f937df155f04e5b5ae0955
alex9 <66374743+piglex9@users.noreply.github.com>
2022-12-31 22:59:46 +0100

    Create hide_nametag.lua

---
+++ hide_nametag.lua
@@ -0,0 +1,7 @@
+local player = game:GetService("Players").LocalPlayer
+
+while true do
+    if player.Character:FindFirstChild("Nametag") then
+        player.Character:FindFirstChild("Nametag"):Destroy()
+    end
+end
